10beb72c617b1a3420473acf6923bd7ccd740f06
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-29 20:10:49 +0700

    membuat akses public

--- solveMultiWaris/solveMultiWaris.cpp
+++ solveMultiWaris/solveMultiWaris.cpp
@@ -33,3 +33,13 @@ public:
 };
 
 class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur) //hal ini dapat dilakukan jiak menggunakan virtual
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
