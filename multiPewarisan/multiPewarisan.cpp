e393e7ad9c4c43d817c9472af6e21cd99cbbfe75
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-29 19:48:28 +0700

    membuat akses publik

--- multiPewarisan/multiPewarisan.cpp
+++ multiPewarisan/multiPewarisan.cpp
@@ -3,3 +3,14 @@
 using namespace std;
 
 class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
