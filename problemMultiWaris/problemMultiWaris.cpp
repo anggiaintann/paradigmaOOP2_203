1337d9c7ea721652b2b832d16a88b8d1f3ddc376
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-29 20:04:01 +0700

    membuat class pekerja

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
+class pekerja : public orang {
